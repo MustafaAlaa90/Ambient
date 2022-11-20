046320e1ec59f61560f55278c23c2c5fb6874047
Mustafa Alaa <mustafa.alaa90@gmail.com>
2022-11-21 01:58:59 +0200

    use pT directly

--- src/ULP.cpp
+++ src/ULP.cpp
@@ -167,7 +167,8 @@ void ULP::getIgas(float pvrev)
 
 void ULP::getConc(float t)
 {
-  float nA = pInA - pIzero * expI(t - pTzero);
+  Serial.printf("pT inside getConc= %f",pT)
+  float nA = pInA - pIzero * expI(pT - pTzero);
   float Sens = pSf /** (1.0 + pTc * (t - 20.0))*/;
   Serial.printf("nA = %f, Sens = %f\n",nA,Sens);
   pX = nA / Sens * 1000.0; // output in ppb
